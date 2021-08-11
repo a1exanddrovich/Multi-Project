784f94eda8f42c03b2fbc9023a546567acdf1b8c
Alexey Bliznichenko <l.bliznichenko@mail.ru>
2021-08-11 14:20:39 +0300

    Added stream instead of loop

--- core/src/main/java/com/epam/task/second/Utils.java
+++ core/src/main/java/com/epam/task/second/Utils.java
@@ -1,17 +1,13 @@
 package com.epam.task.second;
 
 import com.epam.task.first.StringUtils;
+import java.util.stream.Stream;
 
 public class Utils {
 
     public boolean isAllPositiveNumbers(String... str) {
         StringUtils utils = new StringUtils();
-        for (String number : str) {
-            if (!utils.isPositiveNumbers(number)) {
-                return false;
-            }
-        }
-        return true;
+        return Stream.of(str).allMatch(number -> utils.isPositiveNumber(number));
     }
 
 }
